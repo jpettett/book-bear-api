fc71d405ab6399d7c620337a0f178d84a6550aa2
jpettett <joseph.pettett@gmail.com>
2020-02-14 12:05:20 -0800

    Basic file and dependency setup middleware and index.js created

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const booksController = require('./controllers/books');
+const cors = require('cors');
+const methodOverride = require('method-override');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(cors());
+
+app.use('/books', booksController);
+
+app.listen(4000, () => {
+  console.log('connected to port 4000');
+});
