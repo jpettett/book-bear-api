aa978dbf2d876935e8a276ec77cad9799e6b0b37
danielfruth <moto_386@outlook.com>
2020-02-19 13:43:41 -0700

    changed root path to /

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/books', booksController);
+app.use('/', booksController);
 
 app.set('port', process.env.PORT || 4000);
 
