1d783b5f422c0d1d9c3944dde210675c50d5cae6
jmeade11 <hobokenjam@yahoo.com>
2020-02-25 04:38:11 -0500

    Project Submission Feedback
    Excellent job.  I've added inline comments with my suggested
    changes.  They can be found in `index.js` and `controllers/books.js`

--- controllers/books.js
+++ controllers/books.js
@@ -3,43 +3,56 @@ const Book = require('../models/BookSchema');
 
 const router = express.Router();
 
+// The only additions here are the addition of next
+// to prevent the server from hanging and the
+// use of status to set the status for post to 201
+// and delete to 204 no content.
+
 //get all books
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   Book.find({})
     .then(books => res.json(books))
-    .catch(console.error);
+    .catch(next);
 });
 
 //get specified book by id
-router.get('/:id', (req, res) => {
-  Book.findOne({ _id: req.params.id }).then(book => {
-    res.json(book);
-  });
+router.get('/:id', (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => {
+      res.json(book);
+    })
+    .catch(next);
 });
 
 //create a new book
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   const newBook = req.body;
-  Book.create(newBook).then(book => {
-    res.json(book);
-  });
+  Book.create(newBook)
+    .then(book => {
+      res.status(201).json(book);
+    })
+    .catch(next);
 });
 
 //update specified book by id
-router.put('/:id/edit', (req, res) => {
+router.put('/:id/edit', (req, res, next) => {
   const updatedBook = req.body;
   Book.findOneAndUpdate({ _id: req.params.id }, updatedBook, {
     new: true
-  }).then(book => {
-    res.json(book);
-  });
+  })
+    .then(book => {
+      res.json(book);
+    })
+    .catch(next);
 });
 
 //delete specified book by id
-router.delete('/:id', (req, res) => {
-  Book.findOneAndDelete({ _id: req.params.id }).then(book => {
-    res.json(book);
-  });
+router.delete('/:id', (req, res, next) => {
+  Book.findOneAndDelete({ _id: req.params.id })
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch(next);
 });
 
 module.exports = router;
